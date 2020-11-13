c56a2206c539c924e7c2671ed73e0d000decf12d
Student <Student@PLINT-LPT-15>
2020-11-13 13:36:49 +0000

    Add files to chapter 1

---
+++ src/main/java/com/mycompany/chapter1/FirstVariable.java
@@ -0,0 +1,19 @@
+package com.mycompany.chapter1;
+
+public class FirstVariable {
+
+    public static void main(String[] args) {
+     
+        // Declare and initialise
+        //our first variable
+        String message ="Initial Value";
+        System.out.println(message);
+        
+        // Modify the value of the
+        //message variable 
+        message = "Modified Value!";
+        System.out.println(message);
+       
+    } //end main 
+    
+} //end class 
