9f832ab91b344e9a4dd0c30384ffc5359cf6ed05
dazwfd14 <dstones@portlaoisecollege.ie>
2020-11-13 13:48:37 +0000

    Update TestingBranch.java

--- src/main/java/com/mycompany/chapter1/TestingBranch.java
+++ src/main/java/com/mycompany/chapter1/TestingBranch.java
@@ -11,7 +11,7 @@ public class TestingBranch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+     
+    } //end main 
    
 }
