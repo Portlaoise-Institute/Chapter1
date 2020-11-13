9ac28c02c1876709de5257ac88f3db6c11f78542
Darren Stones <dstones@portlaoisecollege.ie>
2020-11-13 13:41:04 +0000

    Add files to chapter 1

--- src/main/java/com/mycompany/chapter1/TestingBranch.java
+++ src/main/java/com/mycompany/chapter1/TestingBranch.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.chapter1;
 
 /**
@@ -17,5 +13,5 @@ public class TestingBranch {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+   
 }
